770c189f0cd82f787290e650412c17b471965e12
Mouhamadou Sylla <syllamouhamed99@gmail.com>
2025-04-28 19:56:22 +0000

    Api response class implemented

--- src/core/ApiResponse.ts
+++ src/core/ApiResponse.ts
@@ -8,22 +8,63 @@ enum ResponseStatus {
     INTERNAL_ERROR = 500
 }
 
-abstract class ApiResponse {
+interface MetaData {
+    timestamp: string;
+    path: string;
+    processingTimeMs?: number;
+}
+
+abstract class ApiResponse<T = any> {
     constructor(
         protected status: ResponseStatus,
-        protected message: string
+        protected message: string,
+        protected data?: T
     ) {}
 
-    protected prepare<T extends ApiResponse>(
+    protected prepare(
         res: Response,
-        response: T,
-        headers: { [key: string]: string }
+        req: Response['req'],
+        headers: { [key: string]: string } = {},
     ): Response {
-        for (const [key, value] of Object.entries(headers)) res.append(key, value);
-        return res.status(this.status)
+        for (const [key, value] of Object.entries(headers)) {
+            res.append(key, value);
+        }
+        return res.status(this.status).json(this.sanitize(req))
     }
 
-    private static sanitize<T extends ApiResponse>(response: T): T {
-        
+    public send(res: Response, req: Response['req'], headers: { [key: string]: string } = {}): Response  {
+        return this.prepare(res, req, headers);
+    }
+
+    private buildMetaData(req: Response['req']): MetaData {
+        return {
+            timestamp: new Date().toISOString(),
+            path: req.originalUrl
+        }
+    }
+
+    private sanitize(req: Response['req']) {
+        const responseBody: Record<string, any> = {
+            message: this.message,
+            meta: this.buildMetaData(req)
+        };
+
+        if (this.data != undefined) {
+            responseBody.data = this.data;
+        }
+
+        return responseBody;
+    }
+}
+
+class SuccessResponse<T> extends ApiResponse<T> {
+    constructor(message: string, data?: T) {
+        super(ResponseStatus.SUCCESS, message, data);
+    }
+}
+
+class FailureResponse extends ApiResponse {
+    constructor(message: string) {
+        super(ResponseStatus.BAD_REQUEST, message, undefined);
     }
 }
